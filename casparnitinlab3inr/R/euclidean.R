84c7d38910287600924e2ae57a66a78ab50adccb
Nitin <datasciencenitinkumar@gmail.com>
2018-09-17 10:09:53 +0200

    Commit-6

---
+++ casparnitinlab3inr/R/euclidean.R
@@ -0,0 +1,18 @@
+euclidiean <- function(x1,x2){
+  
+  is.scalar <- function(femto) is.numeric(femto) && length(femto) == 1L
+  if(is.scalar(x1)==FALSE || is.scalar(x2)==FALSE){
+    stop("The input is not correct")}
+  
+  if(x1>x2){
+    smallern<-x2
+  } else {
+    smallern<-x1
+  }
+  for(i in 1:smallern){
+    if((x1%%i==0)&&(x2%%i==0)){
+      euclid=i
+    }}
+  return(euclid)
+}
+
